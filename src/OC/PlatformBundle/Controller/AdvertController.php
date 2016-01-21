8dac2ccf25b164702c6c778f97cc3687eb78097f
sungvandan <svd.phan@gmail.com>
2016-01-21 08:13:32 +0100

    entity

--- src/OC/PlatformBundle/Controller/AdvertController.php
+++ src/OC/PlatformBundle/Controller/AdvertController.php
@@ -5,8 +5,10 @@ namespace OC\PlatformBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use OC\PlatformBundle\Entity\Advert;
 
 /**
 * 
@@ -21,17 +23,29 @@ class AdvertController extends Controller
 
 	public function viewAction($id)
 	{
-	$advert = array(
-		'title'   => 'Recherche développpeur Symfony2',
-		'id'      => $id,
-		'author'  => 'Alexandre',
-		'content' => 'Nous recherchons un développeur Symfony2 débutant sur Lyon. Blabla…',
-		'date'    => new \Datetime()
-	);
+		$advert = array(
+			'title'   => 'Recherche développpeur Symfony2',
+			'id'      => $id,
+			'author'  => 'Alexandre',
+			'content' => 'Nous recherchons un développeur Symfony2 débutant sur Lyon. Blabla…',
+			'date'    => new \Datetime()
+		);
+		    $repository = $this->getDoctrine()
+	      ->getManager()
+	      ->getRepository('OCPlatformBundle:Advert')
+	    ;
+	    $advert = $repository->find($id);
+	     if (null === $advert) {
+	      throw new NotFoundHttpException("L'annonce d'id ".$id." n'existe pas.");
+	    }
+	        return $this->render('OCPlatformBundle:Advert:view.html.twig', array(
+	      'advert' => $advert
+	    ));
 
-	return $this->render('OCPlatformBundle:Advert:view.html.twig', array(
-	  'advert' => $advert
-	));
+
+		return $this->render('OCPlatformBundle:Advert:view.html.twig', array(
+		  'advert' => $advert
+		));
 	}
 	public function viewSlugAction($year, $slug, $_format)
 	{
@@ -40,8 +54,31 @@ class AdvertController extends Controller
 
 	public function addAction(Request $request)
 	{
-		
-		return $this->render('OCPlatformBundle:Advert:add.html.twig');
+	    // Création de l'entité
+	    $advert = new Advert();
+	    $advert->setTitle('Recherche développeur Symfony2.');
+	    $advert->setAuthor('Alexandre');
+	    $advert->setContent("Nous recherchons un développeur Symfony2 débutant sur Lyon. Blabla…");
+	    $advert->setData(new \DateTime());
+	    // On peut ne pas définir ni la date ni la publication,
+	    // car ces attributs sont définis automatiquement dans le constructeur
+
+	    // On récupère l'EntityManager
+	    $em = $this->getDoctrine()->getManager();
+
+	    // Étape 1 : On « persiste » l'entité
+	    $em->persist($advert);
+
+	    // Étape 2 : On « flush » tout ce qui a été persisté avant
+	    $em->flush();
+
+	    // Reste de la méthode qu'on avait déjà écrit
+	    if ($request->isMethod('POST')) {
+			$request->getSession()->getFlashBag()->add('notice', 'Annonce bien enregistrée.');
+			return $this->redirect($this->generateUrl('oc_platform_view', array('id' => $advert->getId())));
+	    }
+
+	    return $this->render('OCPlatformBundle:Advert:add.html.twig');
 	}
 
 	
---
+++ src/OC/PlatformBundle/Entity/Advert.php
@@ -0,0 +1,241 @@
+<?php
+
+namespace OC\PlatformBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Advert
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="OC\PlatformBundle\Entity\AdvertRepository")
+ */
+class Advert
+{
+
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="date", type="datetime")
+     */
+    private $date;
+
+    /**
+     *@ORM\OneToOne(targetEntity="OC\PlatformBundle\Entity\Image", cascade={"persist"})
+     */
+    private $image;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="title", type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="author", type="string", length=255)
+     */
+    private $author;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="content", type="text")
+     */
+    private $content;
+
+    public function __construct()
+    {
+        $this->date = new \DateTime();    
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set data
+     *
+     * @param \DateTime $data
+     *
+     * @return Advert
+     */
+    public function setData($data)
+    {
+        $this->data = $data;
+
+        return $this;
+    }
+
+    /**
+     * Get data
+     *
+     * @return \DateTime
+     */
+    public function getData()
+    {
+        return $this->data;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     *
+     * @return Advert
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set author
+     *
+     * @param string $author
+     *
+     * @return Advert
+     */
+    public function setAuthor($author)
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    /**
+     * Get author
+     *
+     * @return string
+     */
+    public function getAuthor()
+    {
+        return $this->author;
+    }
+
+    /**
+     * Set content
+     *
+     * @param string $content
+     *
+     * @return Advert
+     */
+    public function setContent($content)
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    /**
+     * Get content
+     *
+     * @return string
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    /**
+     * Set publised
+     *
+     * @param boolean $publised
+     *
+     * @return Advert
+     */
+    public function setPublised($publised)
+    {
+        $this->publised = $publised;
+
+        return $this;
+    }
+
+    /**
+     * Get publised
+     *
+     * @return boolean
+     */
+    public function getPublised()
+    {
+        return $this->publised;
+    }
+
+    /**
+     * Set date
+     *
+     * @param \DateTime $date
+     *
+     * @return Advert
+     */
+    public function setDate($date)
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    /**
+     * Get date
+     *
+     * @return \DateTime
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    /**
+     * Set image
+     *
+     * @param \OC\PlatformBundle\Entity\Image $image
+     *
+     * @return Advert
+     */
+    public function setImage(\OC\PlatformBundle\Entity\Image $image)
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    /**
+     * Get image
+     *
+     * @return \OC\PlatformBundle\Entity\Image
+     */
+    public function getImage()
+    {
+        return $this->image;
+    }
+}
